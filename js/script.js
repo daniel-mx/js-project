1d5886d82782e9a4d5e743bf196791f5961d309a
Daniel <developvxz@gmail.com>
2020-04-23 18:46:10 +0300

    2nd

---
+++ js/script.js
@@ -0,0 +1,26 @@
+for (let i = 0; i < 8; i++) {
+    if (i == 6) {
+        continue;
+    }
+    console.log(i);
+}
+
+/* let i = 0;
+while (i < 8) {
+    if (i == 6) {
+        i++;
+        continue;
+    }
+    console.log(i);
+    i++;
+}
+ */
+/* let i = 0;
+do {
+    if (i == 6) {
+        i++;
+        continue;
+    }
+    console.log(i);
+    i++;
+} while (i < 8) */
